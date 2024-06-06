58411cfa1ca64bb5086b4959588773b8c0940e0d
brighteast99 <brighteast99@gmail.com>
2024-06-06 14:10:07 +0900

    feat: admin 페이지 수정
    이미지 및 템플릿 미리보기 적용

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from mptt.admin import DraggableMPTTAdmin
 from blog.posts.models import Category, Post, Template
 
 
 class CategoryAdmin(DraggableMPTTAdmin):
-    list_display = (
-        'tree_actions',
-        'id', 'indented_title',
-        'cover_image', 'is_deleted', 'is_hidden')
+    list_display = ('tree_actions', 'id', 'indented_title', 'cover', 'is_hidden', 'is_deleted', )
+
+    def cover(self, instance):
+        if instance.cover_image:
+            return format_html(f'<img src="{instance.cover_image.url}" style="max-height:150px"/>')
+        return format_html('<div/>')
+
     mptt_level_indent = 20
 
 
@@ -15,14 +19,17 @@ admin.site.register(Category, CategoryAdmin)
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'category', '__str__', 'is_hidden', 'created_at', 'updated_at', )
+    list_display = ('id', 'category', 'title', 'created_at', 'updated_at', 'is_hidden', 'is_deleted', )
 
 
 admin.site.register(Post, PostAdmin)
 
 
 class TemplateAdmin(admin.ModelAdmin):
-    list_display = ('id', '__str__', 'content', )
+    def preview(self, instance):
+        return format_html(f'<div style="max-height: 300px; overflow-y: auto">{instance.content}</div>')
+
+    list_display = ('id', 'name', 'preview')
 
 
 admin.site.register(Template, TemplateAdmin)
